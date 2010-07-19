333d3599d159f47fa0c6ee06711b9608462d9979
nmohr <>
2010-07-19 16:39:23 +0000

    This commit was manufactured by cvs2svn to create tag 'V00-08-01'.
    Sprout from master 2010-07-19 16:39:22 UTC nmohr 'New muon MC matching'
    Delete:
        DQM/Physics/BuildFile
        DQM/Physics/plugins/plugins.cc
        DQM/Physics/python/susyHadronDQM_cfi.py
        DQM/Physics/python/susyLeptonDQM_cfi.py
        DQM/Physics/src/SusyHadronDQM.h
        DQM/Physics/src/SusyLeptonDQM.h
        DQM/Physics/test/susyDQM_cfg.py
        SUSYBSMAnalysis/SUSYDiLepton/BuildFile
        SUSYBSMAnalysis/SUSYDiLepton/interface/SUSYDiLeptonAnalysis.h
        SUSYBSMAnalysis/SUSYDiLepton/src/SUSYDiLeptonAnalysis.cc
        SUSYBSMAnalysis/SUSYDiLepton/test/SUSYDiLeptonAnalysis.cfg
        SUSYDiLepton/BuildFile
        SUSYDiLepton/data/efficiency_Electron.data
        SUSYDiLepton/data/efficiency_Muon.data
        SUSYDiLepton/interface/Combinations.h
        SUSYDiLepton/interface/SUSYDiLeptonAnalysis.h
        SUSYDiLepton/src/SUSYDiLeptonAnalysis.cc
        SUSYDiLepton/test/SUSYDiLeptonAnalysis.py
        SusyCAF/BuildFile
        SusyCAF/interface/SusyCAF_Electron.h
        SusyCAF/interface/SusyCAF_Event.h
        SusyCAF/interface/SusyCAF_MET.h
        SusyCAF/interface/SusyCAF_Muon.h
        SusyCAF/interface/SusyTree.h
        SusyCAF/plugins/BuildFile
        SusyCAF/plugins/SealModules.cc
        SusyCAF/plugins/SusyCAF_Event.cc
        SusyCAF/plugins/SusyTree.cc
        SusyCAF/python/SusyCAF_Electron_cfi.py
        SusyCAF/python/SusyCAF_Event_cfi.py
        SusyCAF/python/SusyCAF_MET_cfi.py
        SusyCAF/python/ntuple_cff.py
        SusyCAF/python/theBigNtuple_cfi.py
        SusyCAF/test/treeData_cfg.py
        SusyPAT/V8/Jet.h
        SusyPAT/V8/PATJetProducer.cc
        SusyPAT/V8/SUSY_pattuple_cff.py
        SusyPAT/V8/classes_def.xml
        SusyPAT/V8/unresolved_SUSY_pattuple_cfg.py

--- SusyCAF/python/ntuple_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from SUSYBSMAnalysis.SusyCAF.theBigNtuple_cfi import *
-
-#Schedule
-ntuplize_step = cms.Path( theBigNtuple )
-schedule = cms.Schedule( ntuplize_step )
