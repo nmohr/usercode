6b7e5b180b1fb978ced69b5be72932c9eb48ef7d
nmohr <>
2010-11-16 10:18:57 +0000

    This commit was manufactured by cvs2svn to create tag 'V00-09-08'.
    Sprout from master 2010-11-16 10:18:56 UTC nmohr 'Fix for pf'
    Delete:
        DQM/Physics/BuildFile
        DQM/Physics/plugins/plugins.cc
        DQM/Physics/python/susyHadronDQM_cfi.py
        DQM/Physics/python/susyLeptonDQM_cfi.py
        DQM/Physics/src/SusyHadronDQM.h
        DQM/Physics/src/SusyLeptonDQM.h
        DQM/Physics/test/susyDQM_cfg.py
        SUSYBSMAnalysis/SUSYDiLepton/BuildFile
        SUSYBSMAnalysis/SUSYDiLepton/interface/SUSYDiLeptonAnalysis.h
        SUSYBSMAnalysis/SUSYDiLepton/src/SUSYDiLeptonAnalysis.cc
        SUSYBSMAnalysis/SUSYDiLepton/test/SUSYDiLeptonAnalysis.cfg
        SUSYDiLepton/BuildFile
        SUSYDiLepton/data/efficiency_Electron.data
        SUSYDiLepton/data/efficiency_Muon.data
        SUSYDiLepton/interface/Combinations.h
        SUSYDiLepton/interface/SUSYDiLeptonAnalysis.h
        SUSYDiLepton/src/SUSYDiLeptonAnalysis.cc
        SUSYDiLepton/test/SUSYDiLeptonAnalysis.py
        SusyCAF/BuildFile
        SusyCAF/interface/SusyCAF_Electron.h
        SusyCAF/interface/SusyCAF_Event.h
        SusyCAF/interface/SusyCAF_MET.h
        SusyCAF/interface/SusyCAF_Muon.h
        SusyCAF/interface/SusyTree.h
        SusyCAF/plugins/BuildFile
        SusyCAF/plugins/SealModules.cc
        SusyCAF/plugins/SusyCAF_Event.cc
        SusyCAF/plugins/SusyTree.cc
        SusyCAF/python/SusyCAF_Electron_cfi.py
        SusyCAF/python/SusyCAF_Event_cfi.py
        SusyCAF/python/SusyCAF_MET_cfi.py
        SusyCAF/python/ntuple_cff.py
        SusyCAF/python/theBigNtuple_cfi.py
        SusyCAF/test/treeData_cfg.py
        SusyPAT/V8/Jet.h
        SusyPAT/V8/PATJetProducer.cc
        SusyPAT/V8/SUSY_pattuple_cff.py
        SusyPAT/V8/classes_def.xml
        SusyPAT/V8/unresolved_SUSY_pattuple_cfg.py

--- SusyCAF/python/SusyCAF_Electron_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-susycafelectron = cms.EDProducer("SusyCAF_GsfElectron",
-                            InputTag = cms.InputTag('gsfElectrons'),
-                            Prefix = cms.string('electron'),
-                            Suffix = cms.string('gsf')
-                            )
--- SusyCAF/python/SusyCAF_Event_cfi.py
+++
@@ -1,3 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-susycafevent = cms.EDProducer("SusyCAF_Event")
